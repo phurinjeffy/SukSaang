dd9de0dbcf64600572a8eb882ad0a488cf297829
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2024-02-17 14:44:25 +0700

    update backend (separate files + get_user, register working)

--- server/services.py
+++ server/services.py
@@ -1,22 +1,11 @@
-from fastapi import FastAPI, HTTPException
-
-import ZODB, ZODB.FileStorage
-import transaction
+from fastapi import APIRouter, HTTPException
+from database import *
 from models import *
-import BTrees
-import uvicorn
 
-storage = ZODB.FileStorage.FileStorage('mydata.fs')
-db = ZODB.DB(storage)
-connection = db.open()
-root = connection.root
-root.users = BTrees.OOBTree.BTree()
+router = APIRouter()
 
-root.users["username"] = Customer(username="username", password="password")
 
-app = FastAPI()
-
-@app.get("/users/")
+@router.get("/users/")
 async def get_users():
     try:
         users = []
@@ -27,27 +16,23 @@ async def get_users():
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.post("/register/")
+@router.post("/register/")
 async def register_customer(username: str, password: str):
     try:
-        with transaction.manager:
-            if username in root.users:
-                raise HTTPException(status_code=400, detail="User already exists")
-            customer = Customer(username, password)
-            root.users[username] = customer
-            return {"message": "Customer registered successfully"}
+        if username in root.users:
+            raise HTTPException(status_code=400, detail="User already exists")
+        customer = Customer(username, password)
+        root.users[username] = customer
+        connection.transaction_manager.commit()
+        return {"message": "Customer registered successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@app.post("/login/")
+@router.post("/login/")
 async def login(username: str, password: str):
     if username in root.users:
         user = root.users[username]
         if user.password == password:
             return {"message": "Login successful"}
     raise HTTPException(status_code=401, detail="Invalid username or password")
-
-    
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
