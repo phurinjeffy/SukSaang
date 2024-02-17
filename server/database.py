1c9449e620adbc739bb9e3a83a32df9c1ad868cf
HariTTira <haritpao@gmail.com>
2024-02-17 18:27:54 +0700

    Add root.menus

--- server/database.py
+++ server/database.py
@@ -9,6 +9,9 @@ db = ZODB.DB(storage)
 connection = db.open()
 root = connection.root
 
+root.menus = BTrees.OOBTree.BTree()
+root.menus["vegtablePizza"] = MainDish("veg", 10, "jwfiokdc", 10, "maindish",[] )
+
 def init_db():
     global root
     if hasattr(root, 'users'):
@@ -16,10 +19,14 @@ def init_db():
     else:
         print("Initializing database.")
         try:
-            root = connection.root
+            connection = db.open()
+            root = connection.root            
             print("Database loaded from file.")
+
         except Exception as e:
             print("Error loading database from file:", e)
             print("Initializing database with default data.")
             root.users = BTrees.OOBTree.BTree()
+            root.admin = BTrees.OOBTree.BTree()
             root.users["user1"] = Customer(username="user1", password="pass1")
+            root.admin["admin1"] = Admin(username="admin1", password="pass1")
