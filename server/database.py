18aabd130e81e1479b3af981014c93922e45eda0
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2024-02-18 22:41:46 +0700

    add close_db_connection

--- server/database.py
+++ server/database.py
@@ -3,32 +3,42 @@ import ZODB.FileStorage
 import transaction
 from models import *
 import BTrees
+import atexit
 
-storage = ZODB.FileStorage.FileStorage('mydata.fs')
+storage = ZODB.FileStorage.FileStorage("mydata.fs")
 db = ZODB.DB(storage)
 connection = db.open()
 root = connection.root
+
 
 def init_db():
     global root
     print("Initializing database.")
     try:
-        if not hasattr(root, 'users'):
+        if not hasattr(root, "users"):
             root.users = BTrees.OOBTree.BTree()
             # root.users["default"] = Customer(username="default", password="default")
-        if not hasattr(root, 'admins'):
+        if not hasattr(root, "admins"):
             root.admins = BTrees.OOBTree.BTree()
             # root.admins["default"] = Admin(username="default", password="default")
-        if not hasattr(root, 'menus'):
+        if not hasattr(root, "menus"):
             root.menus = BTrees.OOBTree.BTree()
             # root.menus["veggie"] = MainDish(name="veggie", price=10, description="vegan", cost=100, type="vegan", ingredients=[])
         print("Database loaded from file.")
     except Exception as e:
         print("Error loading database from file:", e)
         print("Initializing database with default data.")
-        initialize_default_data(root)
-        
-def initialize_default_data(root):
-    root.users = BTrees.OOBTree.BTree()
-    root.admins = BTrees.OOBTree.BTree()
-    root.menus = BTrees.OOBTree.BTree()
+        root.users = BTrees.OOBTree.BTree()
+        root.admins = BTrees.OOBTree.BTree()
+        root.menus = BTrees.OOBTree.BTree()
+
+
+def close_db_connection():
+    global connection, db
+    transaction.commit()
+    connection.close()
+    db.close()
+    print("Database closed.")
+
+
+atexit.register(close_db_connection)
