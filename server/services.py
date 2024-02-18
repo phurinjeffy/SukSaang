e033da3d0ddb73052db1af0ed89ffe9ff745624a
nownver <urawee.thani@gmail.com>
2024-02-18 21:36:30 +0700

    add food router

--- server/services.py
+++ server/services.py
@@ -4,6 +4,7 @@ from models import *
 
 router = APIRouter()
 
+
 @router.get("/users/")
 async def get_users():
     try:
@@ -13,6 +14,7 @@ async def get_users():
         return {"users": users}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.post("/register/")
 async def register_customer(username: str, password: str):
@@ -25,7 +27,7 @@ async def register_customer(username: str, password: str):
         return {"message": "Customer registered successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
 
 @router.post("/login/")
 async def login(username: str = Body(...), password: str = Body(...)):
@@ -34,6 +36,7 @@ async def login(username: str = Body(...), password: str = Body(...)):
         if user.password == password:
             return {"message": "Login successful"}
     raise HTTPException(status_code=401, detail="Invalid username or password")
+
 
 @router.get("/admins/")
 async def get_admin():
@@ -44,6 +47,7 @@ async def get_admin():
         return {"admins": admins}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @router.post("/register/admin/")
 async def register_admin(username: str, password: str):
@@ -57,27 +61,46 @@ async def register_admin(username: str, password: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @router.post("/login/admin/")
 async def login_admin(username: str, password: str):
     if username in root.admin:
         admin = root.admin[username]
         if isinstance(admin, Admin) and admin.password == password:
             return {"message": "Admin login successful"}
-    raise HTTPException(status_code=401, detail="Invalid username or password for admin")
+    raise HTTPException(
+        status_code=401, detail="Invalid username or password for admin"
+    )
 
 
-@router.get("/menus/")
+@router.get("/admin/menus/")
 async def get_menus():
     try:
         menus = []
         for name, menu in root.menus.items():
-            menus.append({"name": menu.name, "price": menu.price, "description": menu.description, "cost": menu.cost, "ingredients": menu.ingredients})
+            menus.append(
+                {
+                    "name": menu.name,
+                    "price": menu.price,
+                    "description": menu.description,
+                    "cost": menu.cost,
+                    "ingredients": menu.ingredients,
+                }
+            )
         return {"menus": menus}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.post("/menu/")
-async def add_menu(name : str = Body(...), price : int = Body(...), description : str  = Body(...), cost : int  = Body(...), type: str = Body(...), ingredients : list  = Body(...)):
+
+@router.post("/admin/menu/")
+async def add_menu(
+    name: str = Body(...),
+    price: int = Body(...),
+    description: str = Body(...),
+    cost: int = Body(...),
+    type: str = Body(...),
+    ingredients: list = Body(...),
+):
     try:
         if name in root.menus:
             raise HTTPException(status_code=400, detail="Menu already exists")
@@ -87,3 +110,15 @@ async def add_menu(name : str = Body(...), price : int = Body(...), description 
         return {"message": "Menus registered successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/food/")
+async def add_food(
+    name: str = Body(...),
+    price: int = Body(...),
+    description: str = Body(...),
+    cost: int = Body(...),
+    type: str = Body(...),
+    ingredients: list = Body(...),
+):
+    pass
