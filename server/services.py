537b69fd659cf856f37f04932741bdeb6bbd9aae
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2024-02-19 00:55:51 +0700

    update services (user + admin, token)

--- server/services.py
+++ server/services.py
@@ -1,36 +1,41 @@
 import jwt
+import os
+from dotenv import load_dotenv
 from fastapi import HTTPException, Body
 from database import connection
-from models import Customer
+from models import *
 from datetime import datetime, timedelta
 
-# Secret key for JWT token encoding and decoding
-SECRET_KEY = "secret_key_here"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30  
+# ------------------ token ------------------------
+load_dotenv()
 
-# ----------------- customer -----------------------
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
 def create_access_token(username: str):
     expiration_time = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     data = {"sub": username, "exp": expiration_time}
     encoded_token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_token
 
-async def register_customer(username: str, password: str):
+
+# ------------------ user ------------------------
+async def create_user(username: str, password: str):
     try:
-        if username in connection.root.customers:
+        if username in connection.root.users:
             raise HTTPException(status_code=400, detail="User already exists")
-        customer = Customer(username, password)
-        connection.root.customers[username] = customer
+        user = User(username, password)
+        connection.root.users[username] = user
         connection.transaction_manager.commit()
         access_token = create_access_token(username)  # Generate JWT token
         return {"access_token": access_token, "token_type": "bearer"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-async def login_customer(username: str = Body(...), password: str = Body(...)):
-    if username in connection.root.customers:
-        user = connection.root.customers[username]
+async def login_user(username: str = Body(...), password: str = Body(...)):
+    if username in connection.root.users:
+        user = connection.root.users[username]
         if user.password == password:
             access_token = create_access_token(username)  # Generate JWT token
             return {"access_token": access_token, "token_type": "bearer"}
@@ -38,12 +43,12 @@ async def login_customer(username: str = Body(...), password: str = Body(...)):
 
 
 # ------------------ admin ------------------------
-async def register_admin(username: str, password: str):
+async def create_admin(username: str, password: str):
     try:
         if username in connection.root.admins:
-            raise HTTPException(status_code=400, detail="User already exists")
-        customer = Customer(username, password)
-        connection.root.admins[username] = customer
+            raise HTTPException(status_code=400, detail="Admin already exists")
+        admin = Admin(username, password)
+        connection.root.admins[username] = admin
         connection.transaction_manager.commit()
         access_token = create_access_token(username)  # Generate JWT token
         return {"access_token": access_token, "token_type": "bearer"}
